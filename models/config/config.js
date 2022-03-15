f55c3fe2b73facc00577b64efd5f145007834ec3
Sagar-contrivertech <sagar@contrivertech.com>
2022-03-16 00:33:16 +0530

    wishlist

--- models/config/config.js
+++ models/config/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/pixaliveNew', {
+mongoose.connect('mongodb://localhost:27017/pixalive', {
     useNewUrlParser: true
 })
 .then(()=> {
--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const wishlistController = require('../controllers/wishlistController')
+const { isAuthenticated } = require('../middleware/Auth')
 router.put('/wishlist/:id', wishlistController.newwishlist)
 
 //get all products in homepage
-router.get('/allwishlist', wishlistController.Getwishlist)
+router.get('/allwishlist',isAuthenticated, wishlistController.Getwishlist)
 
 router.delete('/allwishlist/:id', wishlistController.deletewishlist)
 
