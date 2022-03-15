937799a58c6b2f491e0dae214dd7c82a4c94b1f9
Sagar-contrivertech <sagar@contrivertech.com>
2022-03-15 22:53:31 +0530

    wishlist

--- models/config/config.js
+++ models/config/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/pixalive', {
+mongoose.connect('mongodb://localhost:27017/pixaliveNew', {
     useNewUrlParser: true
 })
 .then(()=> {
