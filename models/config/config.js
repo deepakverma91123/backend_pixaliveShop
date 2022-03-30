c8ebd9972cc51d25f6294ab5959f4761da2f79eb
Sagar-contrivertech <sagar@contrivertech.com>
2022-03-30 12:40:19 +0530

    changes in database connections

--- models/config/config.js
+++ models/config/config.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+// mongodb://localhost:27017/pixalive'
 
-mongoose.connect('mongodb://localhost:27017/pixalive', {
+mongoose.connect('mongodb+srv://Pixalive:Pixalive1234@cluster0.tpv8o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
 .then(()=> {
