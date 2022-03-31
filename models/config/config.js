c7d8e2f330a5b57908fb1ae1e2a965f22d9bc2a0
Sagar-contrivertech <sagar@contrivertech.com>
2022-03-31 16:20:36 +0530

    chnages in db connections

--- models/config/config.js
+++ models/config/config.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 // mongodb://localhost:27017/pixalive'
 
-mongoose.connect('mongodb+srv://Pixalive:Pixalive1234@cluster0.tpv8o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb+srv://Pixalive:Pixalive1234@cluster0.tpv8o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+//     useNewUrlParser: true
+// })
+// .then(()=> {
+//     console.log("DB connected");
+// })
+// .catch((err)=> {
+//     console.log("DB not connceted", err);
+// })
+
+// 
+
+mongoose.connect('mongodb://localhost:27017/pixalive', {
     useNewUrlParser: true
 })
 .then(()=> {
